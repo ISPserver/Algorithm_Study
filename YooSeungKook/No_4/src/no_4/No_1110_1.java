2be33a1145ab879e3ced22342dfbd3cf10a4c00b
yooseungkook <tmdrnr96@naver.com>
2020-11-23 00:30:21 +0900

    BaekJoon

--- YooSeungKook/No_4/src/no_4/No_1110_1.java
+++ YooSeungKook/No_4/src/no_4/No_1110_1.java
@@ -6,27 +6,29 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 
-public class No_1110 {
+public class No_1110_1 {
 	public static void main(String[] args) throws IOException {
-
-		int count; 
 
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
 		int n = Integer.parseInt(br.readLine());
 
-		while(true) {
+		int cnt = 0;
+		int copy = n;
 
-			if() {
+		while (true) {
+			
+			n = ((n % 10) * 10) + (((n / 10) + (n % 10)) % 10);
+			cnt++;
 
+			if (copy == n) {
+				break;
 			}
+		}
+		bw.write(cnt+"");
 
-
-
-
-		}//while
-
-
+		br.close();
+		bw.close();
 	}//main
 }
